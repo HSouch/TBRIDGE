038f7fd9c6ef66621c1362a374ceaa64c4cfc6b8
HSouch <hsouchereau@outlook.com>
2020-10-14 22:58:59 -0300

    Refined plot method

--- tbridge/plotting/single_bin_plots.py
+++ tbridge/plotting/single_bin_plots.py
@@ -1,10 +1,48 @@
 from matplotlib import pyplot as plt
 from astropy.table import Table
+from numpy import copy, log10
+import tbridge
+
+def adjust_profile(x, y, m_0=27, arcconv=0.168):
+
+    x_new, y_new = copy(x), copy(y)
+    y_new = -2.5 * log10(y / (arcconv ** 2)) + m_0
+
+    return x_new, y_new
 
 
-def single_bin_plot(table_arrays, colours=None):
+def single_bin_plot(table_arrays, colours=None, ind_profile_alpha=1, medians=False, bootstraps=False, ylim=(30, 20),
+                    output="", dpi=150):
 
-    return None
+    plt.figure(figsize=(8, 6))
+
+    for i in range(0, len(table_arrays)):
+        for j in range(0, len(table_arrays[i])):
+            this_prof = table_arrays[i][j]
+
+            sma, intens = this_prof["sma"], this_prof["intens"]
+            sma, intens = adjust_profile(sma, intens)
+
+            plt.plot(sma, intens, c=colours[i], alpha=ind_profile_alpha, lw=1)
+
+        if medians:
+            interps = tbridge.as_interpolations(table_arrays[i])
+            median_sma, median_interp = tbridge.get_median(interps, tbridge.bin_max(table_arrays[i]))
+            median_intens = median_interp(median_sma)
+
+            median_sma, median_intens = adjust_profile(median_sma, median_intens)
+
+            plt.plot(median_sma, median_intens, lw=3, c=colours[i])
+
+    plt.ylim(ylim[0], ylim[1])
+    plt.xlim(1,)
+    plt.xscale("log")
+
+    plt.tight_layout()
+    if output is not "":
+        plt.show()
+    else:
+        plt.savefig(output, dpi=dpi)
 
 
 def single_prof(x, y, error=None):
