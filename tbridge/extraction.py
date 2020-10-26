3730844464ff5dacf514de46fa877e7676ecc5c7
HSouch <hsouchereau@outlook.com>
2020-10-26 04:53:42 -0300

    Added signal alarms to isophote fitting

--- tbridge/extraction.py
+++ tbridge/extraction.py
@@ -10,10 +10,13 @@ except ImportError:
 
 from tqdm import tqdm
 
+import sys
+import signal
+
 import warnings
 
 
-def isophote_fitting(data: ndarray, linear=True):
+def isophote_fitting(data: ndarray, linear=True, use_alarm=False, alarm_time=60, centre_method='standard'):
     """
     Generates a table of results from isophote fitting analysis. This uses photutils Isophote procedure, which is
     effectively IRAF's Ellipse() method.
@@ -26,10 +29,22 @@ def isophote_fitting(data: ndarray, linear=True):
     fail_count, max_fails = 0, 10000
 
     # Get centre of image and cutout halfwidth
-    centre = unravel_index(argmax(data), data.shape)
+    if centre_method == 'standard':
+        centre = (data.shape[0]/2, data.shape[1]/2)
+    elif centre_method == 'max':
+        centre = unravel_index(argmax(data), data.shape)
+    else:
+        centre = (data.shape[0] / 2, data.shape[1] / 2)
+
     cutout_halfwidth = max((ceil(data.shape[0] / 2), ceil(data.shape[1] / 2)))
 
     fitting_list = []
+
+    if use_alarm:
+        original_handler = signal.signal(signal.SIGALRM, TimeoutHandler)
+        signal.alarm(alarm_time)
+    else:
+        original_handler = None
 
     # First, try obtaining morphological properties from the data and fit using that starting ellipse
     try:
@@ -46,35 +61,38 @@ def isophote_fitting(data: ndarray, linear=True):
         fitting_list = flux.fit_image(maxit=100, maxsma=cutout_halfwidth, step=1., linear=linear)
         if len(fitting_list) > 0:
             return fitting_list
+
+    except KeyboardInterrupt:
+        sys.exit(1)
     except RuntimeError:
         fail_count += 1
         if fail_count >= max_fails:
             return []
+    except TimeoutException:
+        signal.signal(signal.SIGALRM, original_handler)
+        pass
     except:
         fail_count += 1
         if fail_count >= max_fails:
             return []
 
+    if use_alarm:
+        signal.alarm(alarm_time)
+
     # If that fails, test a parameter space of starting ellipses
     try:
         for angle in range(0, 180, 45):
             for sma in range(1, 26, 5):
-                geometry = EllipseGeometry(float(centre[0]), float(centre[1]), eps=0.9, sma=sma, pa=angle * pi / 180.)
-                flux = Ellipse(data, geometry)
-                fitting_list = flux.fit_image(maxsma=cutout_halfwidth, step=1., linear=linear)
-                if len(fitting_list) > 0:
-                    return fitting_list
-                geometry = EllipseGeometry(float(centre[0]), float(centre[1]), eps=0.5, sma=sma, pa=angle * pi / 180.)
-                flux = Ellipse(data, geometry)
-                fitting_list = flux.fit_image(maxsma=cutout_halfwidth, step=1., linear=linear)
-                if len(fitting_list) > 0:
-                    return fitting_list
+                for eps in (0.3, 0.5, 0.9):
+                    geometry = EllipseGeometry(float(centre[0]), float(centre[1]), eps=eps,
+                                               sma=sma, pa=angle * pi / 180.)
+                    flux = Ellipse(data, geometry)
+                    fitting_list = flux.fit_image(maxsma=cutout_halfwidth, step=1., linear=linear)
+                    if len(fitting_list) > 0:
+                        return fitting_list
 
-                geometry = EllipseGeometry(float(centre[0]), float(centre[1]), eps=0.3, sma=sma, pa=angle * pi / 180.)
-                flux = Ellipse(data, geometry)
-                fitting_list = flux.fit_image(maxsma=cutout_halfwidth, step=1., linear=linear)
-                if len(fitting_list) > 0:
-                    return fitting_list
+    except KeyboardInterrupt:
+        sys.exit(1)
     except RuntimeError:
         fail_count += 1
         if fail_count >= max_fails:
@@ -83,6 +101,9 @@ def isophote_fitting(data: ndarray, linear=True):
         fail_count += 1
         if fail_count >= max_fails:
             return []
+    except TimeoutException:
+        signal.signal(signal.SIGALRM, original_handler)
+        pass
     except:
         fail_count += 1
         if fail_count >= max_fails:
@@ -91,7 +112,7 @@ def isophote_fitting(data: ndarray, linear=True):
     return fitting_list
 
 
-def extract_profiles(cutout_list, progress_bar=False, linear=True):
+def extract_profiles(cutout_list, progress_bar=False, linear=True, use_alarm=False, alarm_time=60):
     """
     Extract all available profiles
     :param cutout_list: A 2D list of cutouts. The length of each column needs to be the same!
@@ -108,8 +129,7 @@ def extract_profiles(cutout_list, progress_bar=False, linear=True):
         # Iterate through each available object
         local_profiles = []
         for j in range(0, len(cutout_list)):
-
-            t = isophote_fitting(cutout_list[j][index], linear=linear)
+            t = isophote_fitting(cutout_list[j][index], linear=linear, use_alarm=use_alarm, alarm_time=alarm_time)
             if len(t) > 0:
                 local_profiles.append(t.to_table())
 
@@ -127,3 +147,11 @@ def extract_profiles(cutout_list, progress_bar=False, linear=True):
             run_model(i)
 
     return output_profiles
+
+
+class TimeoutException(Exception):
+    pass
+
+
+def TimeoutHandler(signum, frame):
+    raise TimeoutException
