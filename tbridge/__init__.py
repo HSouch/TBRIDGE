86715689816c485388fa5297243f4c367e2924eb
HSouch <hsouchereau@outlook.com>
2020-11-07 04:35:20 -0400

    Added densities import

--- tbridge/__init__.py
+++ tbridge/__init__.py
@@ -1,11 +1,12 @@
 
-from .extraction import *
-from . modelling import *
-from .data import *
-from .config import *
 from .binning import *
+from .config import *
+from .data import *
+from .densities import *
+from .extraction import *
 from .masking import *
 from .medians import *
+from .modelling import *
 from .sims import *
 
 from . import plotting
