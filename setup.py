70a20ff5c566939cc496b20beb0f0aedb67b6b69
HSouch <hsouchereau@outlook.com>
2021-03-17 15:41:02 -0300

    Version update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as readme:
 
 setuptools.setup(
     name="tbridge",
-    version="1.2.21",
+    version="1.3.6",
     author="Harrison Souchereau",
     author_email="harrison.souchereau@yale.edu",
     description="Testing BRIghtness Deviations in Galaxy profile Extractions",
