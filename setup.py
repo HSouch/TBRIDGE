fb4ce14a0a3bc12deb61c7b53e198ae5b58c1290
HSouch <hsouchereau@outlook.com>
2021-05-27 11:13:54 -0300

    Setup update

--- setup.py
+++ setup.py
@@ -7,17 +7,18 @@ with open("README.md", "r") as readme:
 
 setuptools.setup(
     name="tbridge",
-    version="1.4.2",
+    version="1.4.7",
+    licemse="BSD 3-clause 'New' or 'Revised' license",
     author="Harrison Souchereau",
     author_email="harrison.souchereau@yale.edu",
     description="Testing BRIghtness Deviations in Galaxy profile Extractions",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/hsouch/tbridge",
-    packages=setuptools.find_packages(),
     classifiers=[
+        "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: BSD 3-clause 'New' or 'Revised' license",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
